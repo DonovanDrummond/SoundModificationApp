befb1502a3a23541af3a306973716d06f0e1485f
DonovanDrummond <87040020+DonovanDrummond@users.noreply.github.com>
2022-03-27 12:18:18 -0300

    Add project files.

---
+++ SoundModificationApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SoundModificationApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStartAndStopSound = new System.Windows.Forms.Button();
+            this.TracBarPitchInput = new System.Windows.Forms.TrackBar();
+            this.CmbBoxInputDevice = new System.Windows.Forms.ComboBox();
+            this.CmbBoxOutputDevice = new System.Windows.Forms.ComboBox();
+            this.gupBxWaveViewers = new System.Windows.Forms.GroupBox();
+            this.WavViewUnmodeOutput = new NAudio.Gui.WaveViewer();
+            this.WavViewModedOutput = new NAudio.Gui.WaveViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.TracBarPitchInput)).BeginInit();
+            this.gupBxWaveViewers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnStartAndStopSound
+            // 
+            this.BtnStartAndStopSound.Location = new System.Drawing.Point(649, 366);
+            this.BtnStartAndStopSound.Name = "BtnStartAndStopSound";
+            this.BtnStartAndStopSound.Size = new System.Drawing.Size(137, 52);
+            this.BtnStartAndStopSound.TabIndex = 0;
+            this.BtnStartAndStopSound.Text = "Start Sound Capture";
+            this.BtnStartAndStopSound.UseVisualStyleBackColor = true;
+            this.BtnStartAndStopSound.Click += new System.EventHandler(this.BtnStartAndStopSound_Click);
+            // 
+            // TracBarPitchInput
+            // 
+            this.TracBarPitchInput.Location = new System.Drawing.Point(12, 373);
+            this.TracBarPitchInput.Minimum = -10;
+            this.TracBarPitchInput.Name = "TracBarPitchInput";
+            this.TracBarPitchInput.Size = new System.Drawing.Size(614, 45);
+            this.TracBarPitchInput.TabIndex = 1;
+            this.TracBarPitchInput.Scroll += new System.EventHandler(this.TracBarPitchInput_Scroll);
+            this.TracBarPitchInput.ValueChanged += new System.EventHandler(this.TracBarPitchInput_Scroll);
+            // 
+            // CmbBoxInputDevice
+            // 
+            this.CmbBoxInputDevice.FormattingEnabled = true;
+            this.CmbBoxInputDevice.Location = new System.Drawing.Point(16, 18);
+            this.CmbBoxInputDevice.Name = "CmbBoxInputDevice";
+            this.CmbBoxInputDevice.Size = new System.Drawing.Size(172, 21);
+            this.CmbBoxInputDevice.TabIndex = 2;
+            // 
+            // CmbBoxOutputDevice
+            // 
+            this.CmbBoxOutputDevice.FormattingEnabled = true;
+            this.CmbBoxOutputDevice.Location = new System.Drawing.Point(231, 18);
+            this.CmbBoxOutputDevice.Name = "CmbBoxOutputDevice";
+            this.CmbBoxOutputDevice.Size = new System.Drawing.Size(172, 21);
+            this.CmbBoxOutputDevice.TabIndex = 3;
+            // 
+            // gupBxWaveViewers
+            // 
+            this.gupBxWaveViewers.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.gupBxWaveViewers.Controls.Add(this.WavViewUnmodeOutput);
+            this.gupBxWaveViewers.Controls.Add(this.WavViewModedOutput);
+            this.gupBxWaveViewers.Location = new System.Drawing.Point(12, 45);
+            this.gupBxWaveViewers.Name = "gupBxWaveViewers";
+            this.gupBxWaveViewers.Size = new System.Drawing.Size(745, 315);
+            this.gupBxWaveViewers.TabIndex = 4;
+            this.gupBxWaveViewers.TabStop = false;
+            // 
+            // WavViewUnmodeOutput
+            // 
+            this.WavViewUnmodeOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.WavViewUnmodeOutput.Location = new System.Drawing.Point(6, 19);
+            this.WavViewUnmodeOutput.Name = "WavViewUnmodeOutput";
+            this.WavViewUnmodeOutput.SamplesPerPixel = 6;
+            this.WavViewUnmodeOutput.Size = new System.Drawing.Size(733, 129);
+            this.WavViewUnmodeOutput.StartPosition = ((long)(0));
+            this.WavViewUnmodeOutput.TabIndex = 3;
+            this.WavViewUnmodeOutput.WaveStream = null;
+            // 
+            // WavViewModedOutput
+            // 
+            this.WavViewModedOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.WavViewModedOutput.Location = new System.Drawing.Point(6, 171);
+            this.WavViewModedOutput.Name = "WavViewModedOutput";
+            this.WavViewModedOutput.SamplesPerPixel = 6;
+            this.WavViewModedOutput.Size = new System.Drawing.Size(733, 138);
+            this.WavViewModedOutput.StartPosition = ((long)(0));
+            this.WavViewModedOutput.TabIndex = 2;
+            this.WavViewModedOutput.WaveStream = null;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gupBxWaveViewers);
+            this.Controls.Add(this.CmbBoxOutputDevice);
+            this.Controls.Add(this.CmbBoxInputDevice);
+            this.Controls.Add(this.TracBarPitchInput);
+            this.Controls.Add(this.BtnStartAndStopSound);
+            this.Name = "MainForm";
+            this.Text = "Sound Modify";
+            ((System.ComponentModel.ISupportInitialize)(this.TracBarPitchInput)).EndInit();
+            this.gupBxWaveViewers.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStartAndStopSound;
+        private System.Windows.Forms.TrackBar TracBarPitchInput;
+        private System.Windows.Forms.ComboBox CmbBoxInputDevice;
+        private System.Windows.Forms.ComboBox CmbBoxOutputDevice;
+        private System.Windows.Forms.GroupBox gupBxWaveViewers;
+        private NAudio.Gui.WaveViewer WavViewModedOutput;
+        private NAudio.Gui.WaveViewer WavViewUnmodeOutput;
+    }
+}
+
---
+++ SoundModificationApp/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+using System.Windows.Forms;
+
+namespace SoundModificationApp
+{
+    public partial class MainForm : Form
+    {
+        //global variable for the input device
+        WaveInEvent InputDevice;
+        //global variable for the output device
+        WaveOutEvent OutputDevice;
+        //global variable for the  WaveFormat
+        WaveFormat AudioWaveFormat;
+        //global variable for the  SampleRateCal
+        int SampleRateCal;
+        //global variable for the  IsRecording
+        bool IsRecording = false;
+        //global variable for the ModWaveFormat 
+        WaveFormat ModWaveFormat;
+        //global variable for the OutputAudioBuffer
+        BufferedWaveProvider OutputAudioBuffer;
+        public MainForm()
+        {
+            InitializeComponent();
+            //this will set SampleRateCal to value 1000*n more then the default sample rate of the WaveFormat()
+            SampleRateCal = 44000 + (1000 * TracBarPitchInput.Value);
+            //this add all the names for of capable input audio device to CmbBoxInputDevice items
+            for (int counter = 0; counter < WaveIn.DeviceCount; counter++)
+            {
+                CmbBoxInputDevice.Items.Add(WaveIn.GetCapabilities(counter).ProductName);
+            }
+            //this add all the names for of capable output audio device to CmbBoxOutputDevice 
+            for (int counter = 0; counter < WaveOut.DeviceCount; counter++)
+            {
+                CmbBoxOutputDevice.Items.Add(WaveOut.GetCapabilities(counter).ProductName);
+            }
+            //set comboboxs to first item
+            CmbBoxInputDevice.SelectedIndex = 0;
+            CmbBoxOutputDevice.SelectedIndex = 0;
+            //this will create a new wave format for AudioWaveFormat
+            AudioWaveFormat = new WaveFormat();
+            //this will create a new wave format for ModWaveFormat
+            ModWaveFormat = new WaveFormat(SampleRateCal, 2);
+
+        }
+        void StartAndStopCapture()
+        {
+            //case statment to check bool of IsRecording
+            switch (IsRecording)
+            {
+                //if false
+                case false:
+                    {
+                        //this will call StartCapture
+                        StartCapture();
+                        //this will leave the case statment
+                        break;
+                    }
+                //if true
+                case true:
+                    {
+                        //this will call StopCapture
+                        StopCapture();
+                        //this will leave the case statment
+                        break;
+                    }
+            }
+
+          
+
+       
+        }
+        void StopCapture()
+        {
+            //this will stop recording for InputDevice
+            InputDevice.StopRecording();
+            //this will stop playing for OutputDevice
+            OutputDevice.Stop();
+            //next will dispose  OutputDevice and InputDevice
+            OutputDevice.Dispose();
+            InputDevice.Dispose();
+            //this will set IsRecording to false
+            IsRecording = false;
+        }
+        void StartCapture()
+        {
+            //this will create a new in ModWaveFormat
+            ModWaveFormat = new WaveFormat(SampleRateCal, 2);
+            //this will create a new output device using the information from the CmbBoxOutputDevice
+            OutputDevice = new WaveOutEvent() { DeviceNumber = CmbBoxOutputDevice.SelectedIndex };
+            //this will create a new input device using the information from the CmbBoxInputDevice and AudioWaveFormat
+            InputDevice = new WaveInEvent() { DeviceNumber = CmbBoxInputDevice.SelectedIndex, WaveFormat = AudioWaveFormat };
+            //creates a handle for if there is available data and calls InputDevice_DataAvailable
+            InputDevice.DataAvailable += InputDevice_DataAvailable;
+            //this will start the recording for InputDevice
+            InputDevice.StartRecording();
+            //this will create a new BufferedWaveProvider using ModWaveFormat in OutputAudioBuffer
+            OutputAudioBuffer = new BufferedWaveProvider(ModWaveFormat);
+            //this will initiate
+            OutputDevice.Init(OutputAudioBuffer);
+            //this will play the audio from OutputAudioBuffer
+            OutputDevice.Play();
+            //this will set IsRecording to true
+            IsRecording = true;
+        }
+
+
+
+        private void InputDevice_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            //this will add a sample to OutputAudioBuffer
+            OutputAudioBuffer.AddSamples(e.Buffer, 0, e.Buffer.Length);
+            //this will display the sound wave on WavViewModedOutput
+            WavViewModedOutput.WaveStream = new RawSourceWaveStream(e.Buffer, 0 ,e.Buffer.Length, ModWaveFormat);
+            //this will display the sound wave on WavViewUnmodeOutput
+            WavViewUnmodeOutput.WaveStream = new RawSourceWaveStream(e.Buffer, 0, e.Buffer.Length, AudioWaveFormat);
+  
+
+        }
+
+        private void BtnStartAndStopSound_Click(object sender, EventArgs e)
+        {
+            //this will call StartAndStopCapture
+            StartAndStopCapture();
+        }
+
+        private void TracBarPitchInput_Scroll(object sender, EventArgs e)
+        {
+            //this will set SampleRateCal to value 1000*n more then the default sample rate of the WaveFormat()
+            SampleRateCal = 44000 + (1000 * TracBarPitchInput.Value);
+            //next will stop and start the capture to change the audio
+            StopCapture();
+            StartCapture();
+        }
+    }
+}
